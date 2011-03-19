f1df7491b1b62109e52f5d63179f9a3271c365a6
unknown <Pablo@.(none)>
2011-03-19 11:01:05 -0300

    new

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,4 +11,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.cars.build if resource.cars.empty?
   end
   
+  def create
+    puts "#########################################"
+    @user = User.new(params[:user])
+    
+    if @user.save
+        flash[:notice] = "Cliente creado exitosamente"
+        redirect_to  new_workorder_path(:car_id =>@user.cars[0].id)
+    else
+      @user.cars.build unless @user.cars[0].domain
+      @user.build_address unless @user.address
+      render :action => 'new'
+    end
+  end
+  
 end
