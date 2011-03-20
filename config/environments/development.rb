477cdbbcb2eb62f3d2a4db9c7ebb84234654e731
unknown <Pablo@.(none)>
2011-03-20 19:13:23 -0300

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ ComunidadBox::Application.configure do
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'www.comunidadbox.com' }
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
