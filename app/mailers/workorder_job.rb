66a467f4a85a7126c8a4b84707d306d386d675f8
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2011-05-06 10:35:18 -0300

    test

--- app/mailers/workorder_job.rb
+++ app/mailers/workorder_job.rb
@@ -2,11 +2,9 @@ class WorkorderJob
   @queue = :mails
   
   class << self
-    def perform(work_order_id)
-      logger.info "### entro al work order job"
+    def perform(work_order_id)      
       work_order = Workorder.find work_order_id
-      message = WorkOrderJob.delay.notify(work_order)
-      logger.info "############### work order enviada #{work_order.id} a #{work_order.car.user.email}"
+      message = WorkOrderJob.delay.notify(work_order)           
     end
   end
 end
