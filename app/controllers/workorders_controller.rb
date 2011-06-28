bb271f7459beb1fe4081d53ad749d3f43dd32b8f
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2011-06-28 18:31:06 -0300

    test

--- app/controllers/workorders_controller.rb
+++ app/controllers/workorders_controller.rb
@@ -177,8 +177,10 @@ class WorkordersController < ApplicationController
   private
   
   def send_notification(work_order_id)
-    Resque.enqueue WorkorderJob,work_order_id
-    logger.info "### envio de notificacion"
+    #Resque.enqueue WorkorderJob,work_order_id
+    work_order = Workorder.find work_order_id
+    message = WorkOrderNotifier.notify(work_order).deliver
+    logger.info "### envio de notificacion mail"
   end
   
   def sort_column
