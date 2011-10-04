471e39fea9c749a4e9d83f5064859bfbacf4bfb1
unknown <Pablo@.(none)>
2011-10-04 01:09:09 -0300

    Signed-off-by: unknown <Pablo@.(none)>

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ENV['RECAPTCHA_PUBLIC_KEY']  = '6LcoZ8YSAAAAAICZ8SL5fQU1UtBTryKaGTOfqJA4 '
-ENV['RECAPTCHA_PRIVATE_KEY'] = '6LcoZ8YSAAAAAOCSleWlGLVebILh0vNb3eSlmjg0 '
+ENV['RECAPTCHA_PUBLIC_KEY']  = '6Ld3w8gSAAAAAIVDO16qHzYOXYldevcNh9FI33O5'
+ENV['RECAPTCHA_PRIVATE_KEY'] = '6Ld3w8gSAAAAAMD1sWC0zGlRIGzUm4E_NrJHgc63'
 
 # Initialize the rails application
 ComunidadBox::Application.initialize!
