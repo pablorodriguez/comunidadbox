ac15911c4306f1cbf6c0cf200f071edf83dfdd69
unknown <Pablo@.(none)>
2011-10-17 20:48:17 -0300

    Signed-off-by: unknown <Pablo@.(none)>

--- db/migrate/20111008024234_create_notes.rb
+++ db/migrate/20111008024234_create_notes.rb
@@ -7,6 +7,7 @@ class CreateNotes < ActiveRecord::Migration
       t.datetime :start_date
       t.datetime :end_date
       t.integer :status
+      t.integer :creator_id
       t.references :user
       t.references :workorder
       t.references :note
@@ -15,6 +16,7 @@ class CreateNotes < ActiveRecord::Migration
     add_foreign_key(:notes,:users,:dependent => :delete)
     add_foreign_key(:notes,:workorders,:dependent => :delete)
     add_foreign_key(:notes,:notes,:column =>:note_id,:dependent => :delete)
+    add_foreign_key(:notes,:users,:column =>:creator_id,:dependent => :delete)
   end
 
   def self.down
