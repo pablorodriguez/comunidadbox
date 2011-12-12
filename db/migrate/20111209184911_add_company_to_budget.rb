7aa8e24e20b53de5d672ec22b2d834726f8459f8
Pablo Rodriguez <pablo@comunidadbox.com>
2011-12-12 08:12:32 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- db/migrate/20111209184911_add_company_to_budget.rb
+++ db/migrate/20111209184911_add_company_to_budget.rb
@@ -1,14 +1,14 @@
 class AddCompanyToBudget < ActiveRecord::Migration
   def self.up
   	add_column :budgets,:company_id, :integer  	  	
-    add_column :comment, :text
+    add_column :budgets,:comment, :text
   	add_foreign_key(:budgets,:users, :column => 'user_id')
   	add_foreign_key(:budgets,:cars,:dependent =>:delete)
     add_foreign_key(:budgets,:companies,:dependent => :delete)  
   end
 
   def self.down
-    remove_column :comment
+    remove_column :budgets,:comment
   	remove_column :budgets, :company_id  	  	
   end
 end
