ef7c63851f3754a0a79b34302ec00dca37cd62a1
Pablo Rodriguez <pablo@comunidadbox.com>
2011-12-16 04:53:05 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- db/migrate/20111216053016_add_budget_id_to_work_order.rb
+++ db/migrate/20111216053016_add_budget_id_to_work_order.rb
@@ -1,10 +1,10 @@
 class AddBudgetIdToWorkOrder < ActiveRecord::Migration
   def self.up
-  	add_column :workorders,:budget_id, :integer
-  	add_foreign_key(:workorders,:budget,:dependent =>:delete)
+    add_column :workorders,:budget_id, :integer
+    add_foreign_key(:workorders,:budgets,:dependent =>:delete)
   end
 
   def self.down
-  	remove_column :workorders,:budget_id
+    remove_column :workorders,:budget_id
   end
 end
