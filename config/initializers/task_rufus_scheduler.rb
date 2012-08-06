4bae3b5fc37fc1970db78b7a3e607849094c1696
Pablo Rodriguez <pablo@comunidadbox.com>
2012-08-06 02:56:17 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- config/initializers/task_rufus_scheduler.rb
+++ config/initializers/task_rufus_scheduler.rb
@@ -8,13 +8,13 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every("1m") do
   #this will run in every 1 minutes.
   #Envia notificaciones de alarmas
-  Resque.enqueue(AlarmJob)
+  #Resque.enqueue(AlarmJob)
 end
 
 scheduler.every("1d") do
   #this will run in every day.
   #Envias ofertas de servicios
-  Resque.enqueue(ServiceOfferJob)
+  #Resque.enqueue(ServiceOfferJob)
 end
  
 #You can schedule as many job as you want here
