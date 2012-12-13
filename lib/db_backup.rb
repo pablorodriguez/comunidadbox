a592e18ef19618d5faa2618bfce850fb70aa42f2
Pablo Rodriguez <pablo@comunidadbox.com>
2012-12-13 00:40:26 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

---
+++ lib/db_backup.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+databases = {
+	:comunidad_box_dev => {
+	:database => 'comunidadbox_development',
+	:username => 'combox',
+	:password => 'combox'
+	}
+}
+
+db_backups = ARGV.shift 
+day_sec = (60 * 60 * 24)
+
+backup_file =""
+
+#create the db backup
+databases.each do |name,config|
+
+	backup_file = config[:database] + '_' + Time.now.strftime('%Y%m%d%H%M%S')
+
+	mysqldump = "mysqldump -u#{config[:username]} -p#{config[:password]} " + "#{config[:database]}"	
+
+	`#{mysqldump} > #{backup_file}.sql`
+	`gzip #{backup_file}.sql`	
+	`mv #{backup_file}.sql.gz #{db_backups}`
+
+end
+
+#delete backups old more than 2 days
+
+files = Dir.glob("#{db_backups}*.gz")
+files.sort.reverse[10..files.size].each {|f| `rm #{f}` } if files.size > 10
+
