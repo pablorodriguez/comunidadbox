05219cee4e5c4e17f54e1aac143ede656325f9ee
Pablo Rodriguez <pablo@comunidadbox.com>
2012-12-21 09:26:31 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- config/resque_stop.sh
+++ config/resque_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /home/deployer/apps/comunidadbox/current
-kill -QUIT %(cat /home/deployer/apps/comunidadbox/current/tmp/pids/resque_worker_QUEUE.pid)
+kill -QUIT $(cat /home/deployer/apps/comunidadbox/current/tmp/pids/resque_worker_QUEUE.pid)
 rm -f /home/deployer/apps/comunidadbox/current/tmp/pids/resque_worker_QUEUE.pid
