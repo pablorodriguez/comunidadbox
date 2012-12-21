f9819af3a6c758eef6343b19c3f745935f3b6ec1
Pablo Rodriguez <pablo@comunidadbox.com>
2012-12-21 09:43:35 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- config/resque_start.sh
+++ config/resque_start.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 cd /home/deployer/apps/comunidadbox/current
-bundle exec rake resque:work RAILS_ENV=production QUEUE=* VERBOSE=1 PIDFILE=/home/deployer/apps/comunidadbox/current/tmp/pids/resque_worker_QUEUE.pid & >> /home/deployer/app/comunidadbox/current/log/resque_worker_QUEUE.log 2> /home/deployer/apps/comunidadbox/current/log/resque_worker.log
+bundle exec rake resque:work RAILS_ENV=production QUEUE=* VERBOSE=1 PIDFILE=/home/deployer/apps/comunidadbox/current/tmp/pids/resque_worker_QUEUE.pid & >> /home/deployer/apps/comunidadbox/current/log/resque_worker_QUEUE.log 2> /home/deployer/apps/comunidadbox/current/log/resque_worker.log
