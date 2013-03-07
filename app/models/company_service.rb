5be0e06e5306b00e02e29dfc0af79e7cf28abb9d
Pablo Rodriguez <pablo@comunidadbox.com>
2013-03-07 20:30:07 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- app/models/company_service.rb
+++ app/models/company_service.rb
@@ -2,8 +2,8 @@ class CompanyService < ActiveRecord::Base
   belongs_to :company
   belongs_to :service_type
 
-  def self.companies ids
-    CompanyService.where("company_id IN (?)",ids).group(:service_type_id).map(&:service_type)
+  def self.companies(ids)
+    CompanyService.includes(:service_type).where("company_id IN (?)",ids).order("service_types.name").group(:service_type_id).map(&:service_type)
   end
 
   
