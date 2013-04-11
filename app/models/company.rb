6b4023a16f2caae61a24be2b55be84d4fec7352e
Pablo Rodriguez <pablo@comunidadbox.com>
2013-04-11 18:37:26 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- app/models/company.rb
+++ app/models/company.rb
@@ -115,12 +115,18 @@ class Company < ActiveRecord::Base
   end
 
   def self.clients(companies_ids,params)
+
+    first_name = params[:first_name] || ""
+    last_name = params[:last_name] || ""
+    email = params[:email] || ""
+    company_name = params[:company_name] || ""
+    
     page = params[:page] || 1 
     clients = User.includes(:companies_users).where("companies_users.company_id in (?)", companies_ids)
-    clients = clients.where("users.first_name like ?","%#{params[:first_name]}%") if params[:first_name]
-    clients = clients.where("users.last_name like ?","%#{params[:last_name]}%") if params[:last_name]
-    clients = clients.where("users.email like ?","%#{params[:email]}%") if params[:email]
-    clients = clients.where("company_name like ?","%#{params[:company_name]}%") if params[:company_name]    
+    clients = clients.where("users.first_name like ?","%#{params[:first_name]}%") unless first_name.empty?
+    clients = clients.where("users.last_name like ?","%#{params[:last_name]}%") unless last_name.empty?
+    clients = clients.where("users.email like ?","%#{params[:email]}%") unless email.empty?
+    clients = clients.where("company_name like ?","%#{params[:company_name]}%") unless company_name.empty?
     clients.order("users.last_name,users.first_name").paginate(:page =>page,:per_page =>15)    
   end
 
