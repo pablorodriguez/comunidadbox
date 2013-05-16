19ac6150ff9733e7441e07e5eafc62a9619c3a5b
Pablo Rodriguez <pablo@comunidadbox.com>
2013-05-16 16:58:14 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- lib/brands.rb
+++ lib/brands.rb
@@ -1,19 +1,20 @@
-require "watir"
+#!/usr/bin/env ruby
+require "watir-webdriver"
+
+b = Watir::Browser.new :firefox
 
 puts "Setting Firefox as default browser"
-Watir::Browser.default = 'firefox'
-puts "Creating FF instnace"
-b = Watir::Browser.new
-puts "Going to http://www.framfiltros.com.ar/frmresultadosbusquedacatalogo.aspx"
-b.goto "http://www.framfiltros.com.ar/frmresultadosbusquedacatalogo.aspx"
+b.goto "http://deautos.com"
 
-puts "Get all Brands"
-brands = b.select_list(:id,"ctl00_chpResultados_cmbMarcas").getAllContents
+brands = b.select_list(:id => "VehicleBrands").options
+
 brand_id=1
+brands = brands.drop(1)
+puts "Get all Brands #{brands.size}"
 
-File.open("brand-fram.txt", 'w') do |f|
+File.open("brands.txt", 'w') do |f|
   brands.each do |br|
-    f.write("#{brand_id}\t#{br}\n")
+    f.write("#{brand_id}\t#{br.value}\t#{br.text}\n")
     brand_id+=1
   end    
 end
@@ -21,15 +22,19 @@ end
 brand_id=0
 brands.each do |br|
   brand_id+=1
-  puts "Brand ID: #{brand_id}: #{br}"
-  b.select_list(:id,"id_marca").set(br)
-  sleep 5
-  models = b.select_list(:id,"id_modelo").getAllContents
-  models.delete_at 0
-  File.open("brand models.txt", 'a+') do |f|
+  puts "Brand ID: #{brand_id}: #{br.value} #{br.text}"
+  b.select_list(:id,"VehicleBrands").select(br.text)
+  b.driver.manage.timeouts.implicit_wait = 5
+  #Watir::Wait.until { b.select_list(:id => "VehicleModels").selected? '-1' }
+
+  models = b.select_list(:id => "VehicleModels").options
+  models = models.drop(1)
+
+  puts "Got models #{models.size}"
+  File.open("brands_models.txt", 'a+') do |f|
     models.each do |model|
-      f.write("#{brand_id}\t#{model}\n")
-      puts "\t #{brand_id}\t#{model}"      
+      puts "#{br.value}\t#{br.text}\t#{model.text}"
+      f.write("#{br.value}\t#{br.text}\t#{model.text}\n")
     end    
   end
 end
