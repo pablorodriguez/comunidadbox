9b0fe753ccfa425bcf121e0792208898bafe090c
Pablo Rodriguez <pablo@comunidadbox.com>
2013-05-22 17:32:34 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 root = "/home/deployer/apps/comunidadbox/current"
 working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+pid "#{root}/tmp/pids/unicorn_combox.pid"
+stderr_path "#{root}/log/unicorn_combox.log"
+stdout_path "#{root}/log/unicorn_combox.log"
 
 listen "/tmp/unicorn.comunidadbox.sock"
 worker_processes 2
