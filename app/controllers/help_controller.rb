8eab5cf90efd63c388f33711a01b1eae9381bbd4
Pablo Rodriguez <pablo@comunidadbox.com>
2013-06-12 15:01:03 -0300

    Signed-off-by: Pablo Rodriguez <pablo@comunidadbox.com>

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -6,6 +6,7 @@ class HelpController < ApplicationController
     @docs = [
       ["67908748",I18n.t("help_what_is_comunidadbox")],
       ["65332488",I18n.t("help_user_registration")],
+      ["68238819",I18n.t("help_confirm_user")],
       ["67916688",I18n.t("help_new_service_own_car")],
       ["68094773",I18n.t("help_new_client_service_budget")]
     ]
