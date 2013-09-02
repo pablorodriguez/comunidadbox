f457a55618bea1cf8a249bbb92c90dec8a942efd
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2013-09-02 13:38:39 -0300

    Signed-off-by: Pablo Rodriguez <pablorodriguez.ar@gmail.com>

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,6 @@
 class Users::SessionsController < Devise::SessionsController  
+  skip_before_filter :verify_authenticity_token
+  
   include ApplicationHelper
 
   after_filter :set_company_id, :only =>[ :new, :create ]
