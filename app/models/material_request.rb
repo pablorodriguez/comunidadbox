9399ea2df28f4f4024ebf5585ee60ec3f2220673
silvina <silvina@libertad.(none)>
2013-10-15 18:16:52 -0300

    Add validations

--- app/models/material_request.rb
+++ app/models/material_request.rb
@@ -1,6 +1,6 @@
 class MaterialRequest < ActiveRecord::Base
- attr_accessible :cod_provider, :description, :provider, :state, :trademark, :service_type_id
-
+ attr_accessible :cod_provider, :description, :provider, :state, :trademark, :message, :service_type_id, :user_id, :company_id
+  validates_presence_of :provider, :description, :trademark
   belongs_to :user
   belongs_to :company
   belongs_to :service_type
