823452730c782668c4fe26c4aeb2c535f2fe950a
silvina <stenconi@gmail.com>
2013-11-02 12:02:13 -0300

    i18n service_type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   helper :all # include all helpers, all the time
+  before_filter :set_locale
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :secret => 'eee70de2e239c7e16b4f9229f56b8209'
@@ -19,6 +21,18 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|    
     redirect_to root_url, :alert => exception.message
+  end
+   def set_locale
+    I18n.locale = from_http   
+    logger.debug "* Locale set to '#{I18n.locale}'"
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+  end
+  def from_http
+    if respond_to?(:request) && request.env && request.env['HTTP_ACCEPT_LANGUAGE']
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+      #logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+      #logger.debug "* Locale set to '#{I18n.locale}'"
+    end
   end
 end
 
