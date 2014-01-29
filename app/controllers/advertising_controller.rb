b5d4bfd7e81c0999f4b88a647d392af7f7417e60
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2014-01-29 16:06:12 -0300

    Signed-off-by: Pablo Rodriguez <pablorodriguez.ar@gmail.com>

---
+++ app/controllers/advertising_controller.rb
@@ -0,0 +1,5 @@
+class AdvertisingController < ApplicationController
+
+  def index
+  end
+end
---
+++ test/functional/advertising_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AdvertisingControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
