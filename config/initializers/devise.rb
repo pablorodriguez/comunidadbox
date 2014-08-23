39633d2127df6b2bf2fc07ee86c2e2d0b45b77b9
Pablo Rodriguez <pablorodiguez.ar@gmail.com>
2014-08-23 14:35:19 -0300

    fix devise sender email address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "amdin@comunidadbox.com"
+  config.mailer_sender = "admin@comunidadbox.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
