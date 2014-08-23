71d678402273ae7dc65f396ea1535d65d7e2c5a2
Pablo Rodriguez <pablorodiguez.ar@gmail.com>
2014-08-23 11:00:04 -0300

    change mandril conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ ComunidadBox::Application.configure do
     :address   => "smtp.mandrillapp.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => ENV['MANDRILL_USERNAME'],
-    :password  => ENV['MANDRILL_APIKEY'], # SMTP password is any valid API key
+    :user_name => "pablorodriguez.ar@gmail.com",#ENV['MANDRILL_USERNAME'],
+    :password  => "XSvBptz3NPu4krFWTxfVuA", #ENV['MANDRILL_APIKEY'], # SMTP password is any valid API key
     :authentication => 'login' # Mandrill supports 'plain' or 'login'
     #:domain => 'comunidadbox.com' # your domain to identify your server when connecting
   }
