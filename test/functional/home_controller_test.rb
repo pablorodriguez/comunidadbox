1813e6956dd01c9d557c5241b9c9ecc8df0a6414
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2015-04-15 12:33:39 -0300

    fix employee login, fix active company

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  
+  setup do    
+    create_all_default_data    
+    @employer =  create(:gustavo_de_antonio)
+    create_all_company_data @employer.company_id
+    @marcelo =  create(:marcelo_de_antonio)
+  
+  end
+
+  test "employer login" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in @employer
+    get :index
+    assert_response :success
+  end
+
+  test "employee admin login" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in @marcelo
+    get :index
+    assert_response :success
+  end
 
   test "should get index" do
     get :index
