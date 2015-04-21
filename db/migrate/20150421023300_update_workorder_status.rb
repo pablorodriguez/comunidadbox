f7edabf6c2a2925870d3debd23191cc79cb0744a
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2015-04-21 02:53:53 -0300

    fix update work order status

--- db/migrate/20150421023300_update_workorder_status.rb
+++ db/migrate/20150421023300_update_workorder_status.rb
@@ -2,10 +2,10 @@ class UpdateWorkorderStatus < ActiveRecord::Migration
   def change
     #Workorder.where("status_id != 18 or status_id is null").where("performed <= '2015-03-31'")
     total = 0
-    total_record = Workorder.where("(status_id = 16 or status_id is null) and created_at <= ? and company_id IN(3,6,7,12,27)",'2013-01-01').count
+    total_record = Workorder.where("(status_id = 16 or status_id is null) and created_at <= ? and company_id IN(3,6,7,12,27)",'2015-03-31').count
     p "###### Total Record to updates #{total_record} Lote A"
 
-    Workorder.where("(status_id = 16 or status_id is null) and created_at <= ? and company_id IN(3,6,7,12,27)",'2013-01-01').order("created_at ASC").find_in_batches do |batch|
+    Workorder.where("(status_id = 16 or status_id is null) and created_at <= ? and company_id IN(3,6,7,12,27)",'2015-03-31').order("created_at ASC").find_in_batches do |batch|
       batch.each do |workorder|
         workorder.services.each{|service| service.status_id = 18}
         workorder.save
