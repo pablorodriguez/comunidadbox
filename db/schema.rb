178934a3781e2a268ed239ec754fee2f74491c70
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2015-05-09 08:27:52 -0300

    test db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150423143244) do
+ActiveRecord::Schema.define(:version => 20150507195637) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "state_id"
